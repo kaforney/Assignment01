b8b2baf42da2edf59d39b9fb214781740b4252fc
kaforney <kaforney6686@gmail.com>
2018-02-08 18:34:31 -0800

    Steps 1 - 5
    Steps 1-5 in JavaScript added.

---
+++ js/script.js
@@ -0,0 +1,62 @@
+/*eslint-env browser*/
+
+//STEP 1
+/*
+var someMonth;
+function theMonth 		// function to return current month
+currentMonth 		// a constant
+var summerMonth; 		// an array of summer months
+MyLibraryAfunction 	// a function 
+*/
+
+//STEP 2
+/*
+2.8 				// numeric literal 
+"My name is Kim." 			// string literal 
+true 				// Boolean literal 
+null 				// literal null value 
+*/
+
+//STEP 3
+/*
+var complexExpression = 2 * (3 / 4) + 8;
+var anotherComplexExpression = complexExpression + "%" + (4 / 5)
+*/
+
+//STEP 4
+/*
+var firstName
+var lastName
+var address
+var city
+var state
+var zipCode
+var yourAge
+var referralScore
+var mayWeContactYou
+*/
+
+//STEP 5
+/*
+var firstName = "Kimberly";
+var lastName = "Forney";
+var address = "3030 C Street";
+var city = "San Diego";
+var state = "CA";
+var zipCode = "92012";
+var yourAge = 25 + 6;
+var referralScore = totalPoints / 100; 
+var mayWeContactYou = true;
+*/
+
+//STEP 6
+
+var milesDriven = window.prompt("How many miles have you driven?");
+window.console.log("I am just not good enough to display in a browser window.");
+
+
+window.console.log("I am just not good enough to display in a browser window.");
+
+
+window.console.log("I am just not good enough to display in a browser window.");
+
