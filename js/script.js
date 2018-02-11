e157248b3ce9345175cd2804dc2bc75e262eca40
kaforney <kaforney6686@gmail.com>
2018-02-10 16:48:55 -0800

    Completed Steps 10-25
    Added the remainder of steps through completion of the assignment.

--- js/script.js
+++ js/script.js
@@ -12,28 +12,28 @@ MyLibraryAfunction 	// a function
 //STEP 2
 /*
 2.8 				// numeric literal 
-"My name is Kim." 			// string literal 
-true 				// Boolean literal 
+"My name is Kim."			// string literal 
+true				// Boolean literal 
 null 				// literal null value 
 */
 
 //STEP 3
 /*
 var complexExpression = 2 * (3 / 4) + 8;
-var anotherComplexExpression = complexExpression + "%" + (4 / 5)
+var anotherComplexExpression = complexExpression "%" 5;
 */
 
 //STEP 4
 /*
-var firstName
-var lastName
-var address
-var city
-var state
-var zipCode
-var yourAge
-var referralScore
-var mayWeContactYou
+var firstName;
+var lastName;
+var address;
+var city;
+var state;
+var zipCode;
+var yourAge;
+var referralScore;
+var mayWeContactYou;
 */
 
 //STEP 5
@@ -45,18 +45,155 @@ var city = "San Diego";
 var state = "CA";
 var zipCode = "92012";
 var yourAge = 25 + 6;
-var referralScore = totalPoints / 100; 
+var referralScore = (totalPoints / 500) * 100 + "%"; 
 var mayWeContactYou = true;
 */
 
 //STEP 6
+/*
+var milesDriven = "I've driven " + 20 + " miles so far today.";
+window.console.log(milesDriven);
 
-var milesDriven = window.prompt("How many miles have you driven?");
-window.console.log("I am just not good enough to display in a browser window.");
+var tenNine = "Ten is greater than nine is a " + Boolean(10 > 9) + " statement.";
+window.console.log(tenNine);
+
+var booleanNumber = 15 + Boolean(5 + 2 == 7);
+window.console.log(booleanNumber);
+*/
+
+//STEP 7 
+/*
+window.console.log(make);
+var make = "Chevy";
+*/
+
+//STEP 8 
+/*
+// No, the following string is not valid because the double and single quotations are reversed. Fixed version:
+var someString = "Who once said, 'Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.'";
+window.console.log(someString);
+*/
+
+//STEP 9
+/*
+var exampleNull = null;
+window.console.log(exampleNull);
+var exampleUndefined;
+window.console.log(exampleUndefined);
+*/
+
+//STEP 10
+/*
+window.console.log(typeof "This is a string.");
+*/
+/*
+window.console.log(typeof 65);
+*/
+/*
+window.console.log(typeof true);
+*/
+/*
+window.console.log(typeof new Date());
+*/
+/*
+window.console.log(typeof kim);
+*/
+
+//STEP 11
+/*
+window.alert("Hello " + "Zak Ruvalcaba" + ", welcome to the JavaScript class!");
+*/
+
+//STEP 12
+/*
+var name = "Kim";
+window.alert("Hello " + name + ", welcome to the JavaScript class!");
+*/
+
+//STEP 13
+/*
+var name = "Kimberly";
+var course = "JavaScript";
+window.alert("Hello " + name + ", welcome to the " + course + " class!");
+*/
+
+//STEP 14
+/*
+var name = "Kimberly";
+var course = "JavaScript";
+window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+*/
+
+//STEP 15
+/*
+var name = window.prompt("What is your name?");
+window.alert("Hello " + name + ". \nWelcome to the Javascript class!");
+*/
+
+//STEP 16
+/*
+var name = window.prompt("What is your name?");
+var course = window.prompt("What class are you taking?");
+window.alert("Hello " + name + ". \nWelcome to the " + course + " class!");
+*/
+
+//STEP 17
+/*
+x = 10;
+y = 20;
+window.console.log(x + y);
+*/
+
+//STEP 18
+/*
+var x = 20;
+window.console.log(20 + x);
+*/
+
+//STEP 19
+/*
+var x = 20;
+window.console.log(x * 5);
+*/
+
+//STEP 20
+/*
+var x = 20 % 3;
+window.console.log(x / 1);
+*/
+
+//STEP 21
+/*
+var x = 5;
+var y = 2;
+window.console.log(x * y == 10 && x > y);
+*/
+
+//STEP 22
+/*
+var milesDriven = 600;
+var gallons = 20;
+var mpg = milesDriven / gallons;
+window.console.log(mpg == 20 != mpg == 30;
+*/
+
+//STEP 23
+/*
+var widget = new Object();
+window.console.log(typeof widget);
+*/
+
+//STEP 24
+/*
+var widget = new Object("Kim");
+window.console.log(widget instanceof Object);
+*/
+
+//STEP 25
+/*
+var widget = new Object("Kim");
+window.console.log(widget instanceof Number);
+*/
 
 
-window.console.log("I am just not good enough to display in a browser window.");
-
-
-window.console.log("I am just not good enough to display in a browser window.");
 
